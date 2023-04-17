271c6f609b9ce40d2e3f4145787a59edd1441af9
cg5labs <cgscheible@yahoo.com>
2023-04-17 20:50:33 +0800

    templates/aks-cluster.tf - initial

--- templates/aks-cluster.tf
+++ templates/aks-cluster.tf
@@ -1,12 +1,8 @@
 resource "random_pet" "prefix" {}
 
-provider "azurerm" {
-  features {}
-}
-
 resource "azurerm_resource_group" "default" {
   name     = "${random_pet.prefix.id}-rg"
-  location = "West US 2"
+  location = var.location
 
   tags = {
     environment = "Demo"
