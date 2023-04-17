6339ff3fe2840c73c20423842300f9e8cf4f98fd
cg5labs <cgscheible@yahoo.com>
2023-04-17 21:09:10 +0800

    templates/aks-cluster.tf - updates

--- templates/outputs.tf
+++ templates/outputs.tf
@@ -7,12 +7,12 @@ output "kubernetes_cluster_name" {
 }
 
 output "client_certificate" {
-  value     = azurerm_kubernetes_cluster.example.kube_config.0.client_certificate
+  value     = azurerm_kubernetes_cluster.default.kube_config.0.client_certificate
   sensitive = true
 }
 
 output "kube_config" {
-  value = azurerm_kubernetes_cluster.example.kube_config_raw
+  value = azurerm_kubernetes_cluster.default.kube_config_raw
 
   sensitive = true
 }
